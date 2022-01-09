ee65585507290ca2451f01a5ca06ce487387ee1f
raziman <raziman.terra@gmail.com>
2022-01-09 16:42:12 +0800

    add Client

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-import { Client } from "discord.js";
-import { CommandManager } from "@jiman24/commandment";
 import path from "path";
 import { config } from "dotenv";
+import { Client } from "./structure/Client";
 
 config();
 
-const COMMAND_PREFIX = "!";
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const commandManager = new CommandManager(COMMAND_PREFIX);
+export const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
-commandManager.verbose = true;
-commandManager.registerCommands(path.resolve(__dirname, "./commands"));
+client.commandManager.verbose = true;
+client.commandManager.registerCommands(path.resolve(__dirname, "./commands"));
 
-commandManager.registerCommandNotFoundHandler((msg, cmdName) => {
+client.commandManager.registerCommandNotFoundHandler((msg, cmdName) => {
   msg.channel.send(`Cannot find command "${cmdName}"`);
 })
 
-commandManager.registerCommandOnThrottleHandler((msg, cmd, timeLeft) => {
+client.commandManager.registerCommandOnThrottleHandler((msg, cmd, timeLeft) => {
   const time = (timeLeft / 1000).toFixed(2);
   msg.channel.send(`You cannot run ${cmd.name} command after ${time} s`);
 })
 
-commandManager.registerCommandMissingPermissionHandler((msg, perms) => {
+client.commandManager.registerCommandMissingPermissionHandler((msg, perms) => {
   msg.channel.send(`Missing permissions \`${perms.join(", ")}\``);
 })
 
 client.on("ready", () => console.log(client.user?.username, "is ready!"))
-client.on("messageCreate", msg => commandManager.handleMessage(msg));
+client.on("messageCreate", msg => client.commandManager.handleMessage(msg));
 
 client.login(process.env.BOT_TOKEN);
---
+++ src/structure/Client.ts
@@ -0,0 +1,9 @@
+import { CommandManager } from "@jiman24/commandment";
+import Enmap from "enmap";
+import { Client as DiscordClient } from "discord.js";
+
+
+export class Client extends DiscordClient {
+  commandManager = new CommandManager(process.env.PREFIX || "!");
+  nft = new Enmap("nft");
+}
