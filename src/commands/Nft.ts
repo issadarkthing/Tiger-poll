d939b75036efba8dfaada4d4eba2cc51837ecfb5
raziman <raziman.terra@gmail.com>
2022-01-11 15:47:37 +0800

    add NFT command

---
+++ src/commands/Nft.ts
@@ -0,0 +1,28 @@
+import { Command } from "@jiman24/commandment";
+import { Message, MessageEmbed } from "discord.js";
+import { client } from "..";
+import { Pagination } from "../structure/Pagination";
+
+
+export default class extends Command {
+  name = "nft";
+
+  async exec(msg: Message) {
+
+    const nfts = client.nft.map((x: string, id: number | string) => {
+      const votes = client.vote.get(id) || 0;
+      const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setImage(x)
+        .addField("ID", `${id}`, true)
+        .addField("Votes", `${votes}`, true)
+
+      return embed;
+    });
+
+
+    const menu = new Pagination(msg, nfts);
+
+    await menu.run();
+  }
+}
