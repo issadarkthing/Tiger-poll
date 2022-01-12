76488a35c6b85c70b4bbbdd8b6c7426adcaa5cdf
raziman <raziman.terra@gmail.com>
2022-01-12 11:31:10 +0800

    make combination function more efficient

--- src/utils.ts
+++ src/utils.ts
@@ -1,30 +1,18 @@
 
 
 /** creates only 2d combination */
-export function combination(ls: string[]) {
-  const result: [string, string][] = [];
+export function combination<T>(ls: T[]): [T, T][] {
+
+  const result = new Set<string>();
 
   for (let i = 0; i < ls.length; i++) {
     const current = ls[i];
     const rest = ls.filter(x => x !== current);
 
     for (const other of rest) {
-      result.push([current, other]);
+      result.add(JSON.stringify([current, other].sort()));
     }
   }
 
-  return result
-    .map(x => x.sort())
-    .reduce((acc, v) => {
-
-      for (const pair of acc) {
-        if (pair.includes(v[0]) && pair.includes(v[1])) {
-          return acc;
-        }
-      }
-
-      return [...acc, v];
-
-    }, [] as [string, string][])
+  return [...result.values()].map(x => JSON.parse(x));
 }
-
